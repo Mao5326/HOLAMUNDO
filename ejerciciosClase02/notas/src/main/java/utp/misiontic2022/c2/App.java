2f25278aca436c1e797e3df4c6fd654e03f98d1f
Mao5326 <marico2121@me.com>
2021-07-17 18:13:49 -0500

    promedio_notas

--- ejerciciosClase02/notas/src/main/java/utp/misiontic2022/c2/App.java
+++ ejerciciosClase02/notas/src/main/java/utp/misiontic2022/c2/App.java
@@ -1,16 +1,29 @@
 package utp.misiontic2022.c2;
-import java.util.function.DoubleBinaryOperator;
 
 public class App 
 {
-    public static void main( String[] args )
+public static void main( String[] args )
     {
-        double auxnota=Double.parseDouble(args[0]);
-        for(int i=1; i<args.length; i++){
+        Double auxnota=Double.parseDouble(args[0]);
+
+        Double promedio = 0.0;
+        Integer indnota=1;
+        
+        for(int i=1; i < args.length; i++){
             if(Double.parseDouble(args[i]) < auxnota){
                 auxnota = Double.parseDouble(args[i]);
             }
         }
-        System.out.println(auxnota);
+        args[indnota] = "0";
+        for(int i=2; i<args.length; i++){
+            promedio += Double.parseDouble(args[i]);
+                
+         }
+        promedio /= args.length -2;
+
+System.out.printf("el promedio del estudiante ID %s es: %.2f", args[0], promedio);
+        
     }
+        
 }
+
